7f9e0a3801197f235377c8d7542afc514e6215d6
Jakub Romanek <jakub.romanek12345@gmail.com>
2023-02-10 10:57:23 +0100

    Add first page structure and navbars

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-
+import './styles.css'
 import Episode from './Episode';
 
 const App = () => {
@@ -19,15 +19,44 @@ const App = () => {
     }, []);
 
     return (
+
     <div className='app'>
-        <h1>Episodes of the 4th season of the series Rick and Morty</h1>
+        <nav className='navbar_top'>
+            <div className='navbar__container'>
+                <h2>LOREM IPSUM</h2>
+            </div>
+        </nav>
+    
         <div className='container'>
-            {episodes.map((episode) => {
-                return(
-                    <Episode episode={episode}/>
-                );
-            })}
+            <div className='left_side'>
+                <div className='main_text'>
+                    <span className='text1'>Episodes of the <b>4th</b> </span>
+                    <span className='text1'> season of the series </span>
+                    <span className='text2'>Rick and Morty</span>
+                </div>
+                <img className='image' src={require('./image.png')} alt="Rick&Morty" />
+            </div>
+            <div>
+                {episodes.map((episode) => {
+                    return(
+                        <Episode episode={episode}/>
+                    );
+                })}
+            </div>
         </div>
+
+        <nav className='navbar_bottom'>
+            <div className='navbar__container'>
+                <p
+                    style={{
+                        fontSize: '16px',
+                        color: 'white',
+                    }}
+                >
+                    LOREM IPSUM ©2023
+                </p>
+            </div>
+        </nav>
     </div>
     );
 
